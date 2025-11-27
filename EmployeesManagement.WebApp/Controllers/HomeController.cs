0d041ff32d9d9510c1cae9973b2bc4963899bb95
Hamza Abd-El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2025-11-27 22:56:45 +0200

    Add project files.

---
+++ EmployeesManagement.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using EmployeesManagement.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesManagement.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
